b97c807e577bf7f1eda7e390441581fd2ee97123
Botir <tiboruz@gmail.com>
2025-11-19 14:31:07 +0500

    frerferfqerferg

--- app/Http/Controllers/Api/WoltController.php
+++ app/Http/Controllers/Api/WoltController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Wolt;
+use Illuminate\Support\Facades\Http;
 
 class WoltController extends Controller
 {
@@ -77,7 +78,33 @@ class WoltController extends Controller
 
         $data=$request->all();
         try{
-            $newToken=Wolt::create($data);
+            $request=Http::withHeaders([
+                'Content-Type' => 'application/json',
+                'Accept' => 'application/json',
+                'X-API-Key'=>$headerKey])
+                ->withBody(json_encode($data))
+                ->post("http://213.230.99.65:8080/api/wolt/authorize");
+            if($request->successful()){
+                switch($request->status()){
+                    case 200:
+                        return response()->json(["status"=>"success"],200);    
+                    break;
+                    case 401:
+                        return response()->json([
+                            'status'  => 'error',
+                            'message' => 'missing X-API-Key',
+                        ], 401); // 401 Unauthorized
+                    break;
+                    case 500:
+                        return response()->json(["status"=>"error"],500);    
+                    break;
+                    default:
+                        return response()->json(["status"=>"error"],500);
+                    break;
+                }
+
+            }
+
             return response()->json(["status"=>"success"],200);
 
         }catch(\Exception $e){
